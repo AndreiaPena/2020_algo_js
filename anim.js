bd1ed41016020b3134d140c1236798d688a9a90b
Andreia Pena <andreia.pena.ferreira@gmail.com>
2020-03-13 12:29:33 +0100

    ajout du calcul de a et b sur js

--- anim.js
+++ anim.js
@@ -5,3 +5,16 @@ console.log ("Connected");
     console.log ("Not Authorized")
 }
 
+let a = 8;
+b = a+1;
+a++;
+a *= 2;
+b /= 5;
+b *= b;
+a += 3;
+ 
+if (a=1){
+   console.log (a +=2);
+} else {
+   console.log (a +=3)
+}
